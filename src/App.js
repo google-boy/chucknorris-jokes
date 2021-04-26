448ace59a18b6e77fe5032002ac906a59880ad16
google-boy <otienoh13@gmail.com>
2021-04-26 21:15:20 +0300

    Code refactor.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <aside>
-        <Category></Category>
+        <Category />
       </aside>
     </section>
   );
